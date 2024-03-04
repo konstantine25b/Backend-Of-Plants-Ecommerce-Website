c9a0603773a2352f273e064d4e983c8e7a8be47e
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-04 19:51:29 +0400

    added basic serializers

---
+++ plants/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import CustomUser, Category, Product, Order, OrderItem, Review
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'address']
+        read_only_fields = ['role']
+
+class VendorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'address']
+        read_only_fields = ['role']
+
+class AdminSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'address']
+        read_only_fields = ['role']
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
