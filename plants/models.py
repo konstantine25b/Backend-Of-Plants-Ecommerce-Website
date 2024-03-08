db5f9fd2d36f08ea10b4368a6835a348c4c7c093
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-08 13:24:16 +0400

    created urls.py

--- plants/models.py
+++ plants/models.py
@@ -1,6 +1,1 @@
-from .models.CustomUser import CustomUser
-from .models.Category import Category
-from .models.Product import Product
-from .models.Order import Order
-from .models.OrderItem import OrderItem
-from .models.Review import Review
+
