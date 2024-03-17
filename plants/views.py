5b3ee3507800b776323ad19ccdad95b102e614ff
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-18 00:18:22 +0400

    added filter

--- plants/views.py
+++ plants/views.py
@@ -72,36 +72,39 @@ class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsVendorOrAdminOrReadOnly]
+    
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
+    
+    #es ro vinc qmnis is ikos vendorad
     def perform_create(self, serializer):
         serializer.save(vendor=self.request.user)
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_class = ProductFilter
     
-    # def list(self, request, *args, **kwargs):
-    #     # Perform filtering
-    #     queryset = self.filter_queryset(self.get_queryset())
+    # es redisistvis
+    def list(self, request, *args, **kwargs):
+        # Perform filtering
+        queryset = self.filter_queryset(self.get_queryset())
 
-    #     # Define cache key and time
-    #     cache_key = "product_list"
-    #     cache_time =  300  # 5 minutes
+        # Define cache key and time
+        cache_key = "product_list"
+        cache_time =  300  # 5 minutes
 
-    #     # Attempt to get data from cache
-    #     data = cache.get(cache_key)
+        # Attempt to get data from cache
+        data = cache.get(cache_key)
 
-    #     if not data:
-    #         # Serialize queryset
-    #         data = ProductSerializer(queryset, many=True).data
+        if not data:
+            # Serialize queryset
+            data = ProductSerializer(queryset, many=True).data
 
-    #         # Cache the data
-    #         cache.set(cache_key, data, cache_time)
+            # Cache the data
+            cache.set(cache_key, data, cache_time)
 
-    #     return Response(data)
+        return Response(data)
     
-   
-class ProductDetailView(generics.RetrieveAPIView):
+class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsVendorOrAdminOrReadOnly]
     
     
 # Order views
