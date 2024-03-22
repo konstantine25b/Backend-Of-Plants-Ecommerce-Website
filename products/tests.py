2fde29a8cfb73a544d7ab985513895903fe7c498
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-22 12:19:14 +0400

    added products tests and fixed bug

--- products/tests.py
+++ products/tests.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from .models import Category
 from django.contrib.auth import get_user_model
+from .models import Product
 
 User = get_user_model()
 
@@ -108,3 +109,90 @@ class CategoryAPITest(TestCase):
         
         
 # Products Tests
+
+class ProductAPITest(TestCase):
+    def setUp(self):
+        
+        self.client = APIClient()
+        self.vendor_user = User.objects.create_user(username='vendor', email='vendor@example.com', password='vendorpassword', role='Vendor')
+        self.admin_user = User.objects.create_superuser(username='admin', email='admin@example.com', password='adminpassword', is_staff=True, role='Admin')
+        
+        # Create a category
+        self.category = Category.objects.create(title="Test Category", description="Category for testing")
+        
+        # Create products associated with the category
+        self.product1 = Product.objects.create(vendor=self.vendor_user, category=self.category, title="Product 1", description="Description 1", price=10.00, quantity=5, image_url="https://example.com/image.jpg")
+        self.product2 = Product.objects.create(vendor=self.vendor_user, category=self.category, title="Product 2", description="Description 2", price=15.00, quantity=10, image_url="https://example.com/image2.jpg")
+
+
+    def test_list_products(self):
+        response = self.client.get(reverse('product-list-create'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 2)  # Assuming there are two products in the database
+
+    def test_retrieve_product(self):
+        response = self.client.get(reverse('product-detail', kwargs={'pk': self.product1.pk}))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], "Product 1")
+
+    def test_unauthorized_create_product(self):
+        data = {'title': 'New Product', 'description': 'New Description', 'price': 20.00, 'quantity': 8, 'image_url': 'https://example.com/new_image.jpg'}
+        response = self.client.post(reverse('product-list-create'), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_unauthorized_update_product(self):
+        data = {'title': 'Updated Product', 'description': 'Updated Description', 'price': 25.00, 'quantity': 15, 'image_url': 'https://example.com/updated_image.jpg'}
+        response = self.client.put(reverse('product-detail', kwargs={'pk': self.product1.pk}), data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_unauthorized_delete_product(self):
+        response = self.client.delete(reverse('product-detail', kwargs={'pk': self.product1.pk}))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    
+    def test_vendor_create_product(self):
+         # Authenticate as vendor user
+         self.client.force_authenticate(user=self.vendor_user)
+
+         # Get the primary key values for an existing category
+         category_pk = self.category.pk
+         
+         data = {'title': 'New Product', 'description': 'New Description', 'price': 20.00, 'quantity': 8, 'image_url': 'https://example.com/new_image.jpg', 'vendor': self.vendor_user.pk, 'category': category_pk}
+         response = self.client.post(reverse('product-list-create'), data)
+         
+         if response.status_code != status.HTTP_201_CREATED:
+             print(response.content)
+     
+         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+     # Test authorized update product
+    def test_authorized_update_product(self):
+        # Authenticate as admin user
+        self.client.force_authenticate(user=self.admin_user)
+        category_pk = self.category.pk 
+        # Update product data
+        data = {'title': 'Updated Product', 'description': 'Updated Description', 'price': 25.00, 'quantity': 15, 'image_url': 'https://example.com/updated_image.jpg', 'vendor': self.vendor_user.pk, 'category': category_pk}
+        response = self.client.put(reverse('product-detail', kwargs={'pk': self.product1.pk}), data)
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'Updated Product')
+
+    # Test authorized delete product
+    def test_authorized_delete_product(self):
+        # Authenticate as admin user
+        self.client.force_authenticate(user=self.admin_user)
+
+        # Delete product
+        response = self.client.delete(reverse('product-detail', kwargs={'pk': self.product1.pk}))
+        
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+     #Test retrieving products by category
+    def test_list_products_by_category(self):
+        response = self.client.get(reverse('product-list-create') + f'?category={self.category.pk}')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 2)
+
+    # Test filtering products by price range
+    def test_filter_products_by_price_range(self):
+        response = self.client.get(reverse('product-list-create') + '?price__gte=10.00&price__lte=15.00')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['results']), 2)
--- products/views.py
+++ products/views.py
@@ -7,6 +7,7 @@ from .serializers import (
     CategorySerializer, ProductSerializer,
 
 )
+
 from .permissions import (  CustomCategoryPermission, 
       IsVendorOrAdminOrReadOnly)
 from django.core.cache import cache
@@ -29,44 +30,39 @@ class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
 # Product views
 # # The `ProductListCreateView` class in Python defines a view for listing and creating products with
 # caching implemented for the product list.
+
+
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsVendorOrAdminOrReadOnly]
-    
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProductFilter
-    
-    #es ro vinc qmnis is ikos vendorad
+  
+
+    # Custom logic for creating products
     def perform_create(self, serializer):
-      if self.request.user.role == 'Vendor':
-        # If the user is a vendor, assign the product to the vendor
-        serializer.save(vendor=self.request.user)
-      elif self.request.user.is_staff:
-        # If the user is staff, check if a vendor ID is provided in the request data
-        vendor_id = self.request.data.get('vendor')
-        if vendor_id:
-             vendor = CustomUser.objects.get(id=vendor_id)
-             serializer.save(vendor=vendor)
-        else:
-            # If no vendor ID is provided, assign the product to the staff user
+        if self.request.user.role == 'Vendor':
             serializer.save(vendor=self.request.user)
-    
-    # es redisistvis
+        elif self.request.user.is_staff:
+            vendor_id = self.request.data.get('vendor')
+            if vendor_id:
+                vendor = CustomUser.objects.get(id=vendor_id)
+                serializer.save(vendor=vendor)
+            else:
+                serializer.save(vendor=self.request.user)
+
+  
+     # Custom logic for listing products
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
-        
-        # Serialize filtered queryset
-        data = ProductSerializer(queryset, many=True).data
-        
-        # Define cache key and time
+        page = self.paginate_queryset(queryset)      
+        data = ProductSerializer(page, many=True).data
         cache_key = "product_list"
-        cache_time = 300  # 5 minutes
-        
-        # Cache the data
+        cache_time = 300
         cache.set(cache_key, data, cache_time)
-        
-        return Response(data)
+        return self.get_paginated_response(data)
+    
     
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
