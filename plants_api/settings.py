fa627663e86fdbd88070661215a02a611583372c
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-24 23:00:51 +0400

    changed

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')# es davamate statikuri filebis shesanaxad
+STATIC_ROOT = 'static/'# es davamate statikuri filebis shesanaxad
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
