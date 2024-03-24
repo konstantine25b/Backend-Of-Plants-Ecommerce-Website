3432b2cf26f00680431fbad4e89127c5050ff97d
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-24 16:21:09 +0400

    fixing bugs

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -13,7 +13,7 @@ from datetime import timedelta
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key_if_not_set')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
@@ -101,7 +101,6 @@ DATABASES = {
         'PORT': os.getenv('DATABASE_PORT'),
         
     },
-    
 }
 
 
