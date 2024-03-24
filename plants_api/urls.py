2ac17f16ee0a1f11ed6b37a7a06e888256205c25
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-24 10:10:22 +0400

    added docker and gunicorn

--- plants_api/urls.py
+++ plants_api/urls.py
@@ -7,6 +7,8 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 
@@ -22,4 +24,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+]+ static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
