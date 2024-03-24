56de2244cd94da329561bb64727a8c5ff3d9deb2
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-24 19:17:42 +0400

    adde nginx

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
-    'plants',
     'users',
     'products',
     'orders',
