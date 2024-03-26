5ca59f7d8829674f76010b5cc196ff92d9399662
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-27 03:36:58 +0400

    dsfsd

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -142,13 +142,13 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'whole-static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'whole-media')
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+   os.path.join(BASE_DIR, "staticfiles"),
 ]
 
 # Default primary key field type
