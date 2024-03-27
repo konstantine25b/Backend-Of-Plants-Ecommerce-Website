9cac66d00a14064729de8d35f78394536d88209e
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-27 19:15:35 +0400

    dfsd

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -142,8 +142,8 @@ USE_TZ = True
 
 
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = 'static/'
+STATIC_ROOT =BASE_DIR/'static'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media/')
