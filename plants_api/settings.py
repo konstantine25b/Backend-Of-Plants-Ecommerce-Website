eb41ebafeb1bf316bd14f6a593287d685011ecd3
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-03-27 15:41:12 +0400

    f

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -142,15 +142,8 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'whole-static')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'whole-media')
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = "static/"
+STATIC_ROOT = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
