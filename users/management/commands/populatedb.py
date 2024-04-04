b88a6255503d5572062f41450bf54665315d4261
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-04-04 08:56:51 +0400

    dsfd

--- users/management/commands/populatedb.py
+++ users/management/commands/populatedb.py
@@ -38,7 +38,7 @@ class Command(BaseCommand):
         vendors = CustomUser.objects.filter(role='Vendor') 
         subcategories = SubCategory.objects.all()
         
-        for _ in range(400):
+        for _ in range(1200):
             vendor = random.choice(vendors)
             subcategory = random.choice(subcategories)
 
@@ -59,7 +59,7 @@ class Command(BaseCommand):
         customers = CustomUser.objects.filter(role='Customer') 
         products = Product.objects.all()
         
-        for _ in range(215):  # Generate 5 orders
+        for _ in range(315):  # Generate 5 orders
                 customer = random.choice(customers)
     
                 order = Order.objects.create(
@@ -83,7 +83,7 @@ class Command(BaseCommand):
             
                 order.save()
                 
-        for _ in range(1430):  # Generate between 0 and 5 reviews
+        for _ in range(1630):  # Generate between 0 and 5 reviews
                 customer = random.choice(customers)
                 rating = random.randint(1, 5)
                 comment = fake.text()
