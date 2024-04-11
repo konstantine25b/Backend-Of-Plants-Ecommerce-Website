9744f74cfc4cbbd665fb53374e2a35f4d78c0f8a
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-04-11 09:47:08 +0400

    dfd

--- plants_api/settings.py
+++ plants_api/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key_if_not_set')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['164.92.170.208', 'localhost', '127.0.0.1', 'apikosaplants.online','api.apikosaplants.online']
+ALLOWED_HOSTS = ['164.92.170.208', 'localhost', 'apikosaplants.online']
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1' , '178.128.194.208']
 CORS_ALLOW_ALL_ORIGINS =True # es ro yvelgan gaeshvas jerjerobit
 
