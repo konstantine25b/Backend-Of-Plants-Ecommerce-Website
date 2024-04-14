358319ab267e6c604b101cb81863e086adbda0f9
konstantine25b <102245292+konstantine25b@users.noreply.github.com>
2024-04-14 19:22:59 +0400

    deleted unneccessary staff

--- plants/admin.py
+++
@@ -1,9 +0,0 @@
-# from django.contrib import admin
-# from .models import *
-
-# admin.site.register(CustomUser)
-# admin.site.register(Category)
-# admin.site.register(Product)
-# admin.site.register(Order)
-# admin.site.register(OrderItem)
-# admin.site.register(Review)
